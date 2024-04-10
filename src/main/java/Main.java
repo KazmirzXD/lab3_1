991ae8f1cccc66a360778ba4fc6725b6228d6323
KazmirzXD <161707953+KazmirzXD@users.noreply.github.com>
2024-04-10 12:18:48 +0000

    3_1_kwadrat5x5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,17 @@
-public class Square {
-    public static void drawSquare(int size) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
-                    System.out.print("*");
+
+public class Main {
+    public static void main(String[] args) {
+        int szer = 7;
+        int wys = 5;
+        for (int i = 0; i < wys; i++) {
+            for (int j = 0; j < szer; j++) {
+                if (i == 0 || i == wys - 1 || j == 0 || j == szer - 1) {
+                    System.out.print('*');
                 } else {
-                    System.out.print(" ");
+                    System.out.print(' ');
                 }
             }
             System.out.println();
         }
-    }
-
-    public static void main(String[] args) {
-        drawSquare(5); // Change the parameter to adjust the size of the square
     }
 }
