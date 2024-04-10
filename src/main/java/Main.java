b84cded44bceb2bfe14195592bfd139935b8aa68
KazmirzXD <161707953+KazmirzXD@users.noreply.github.com>
2024-04-10 12:11:12 +0000

    3_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,18 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+public class Square {
+    public static void drawSquare(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 
-// import org.junit.jupiter.api.Test;
-
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
-
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+    public static void main(String[] args) {
+        drawSquare(5); // Change the parameter to adjust the size of the square
+    }
 }
